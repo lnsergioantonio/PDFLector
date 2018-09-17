f82ff0d0e9007f6d84b28348f8174d1d8e0ca472
sergio <lnsergioantonio@gmail.com>
2018-09-17 00:37:49 -0500

    finish

--- PDFLector/ViewController2.swift
+++ PDFLector/ViewController2.swift
@@ -20,7 +20,11 @@ class ViewController2: UIViewController {
 
     func showPdf(){
         // get direction pdf
-        
+        let pathPdf = URL(fileURLWithPath: Bundle.main.path(forResource: namePdf!, ofType: "pdf", inDirectory: "PDF")!)
+        // transform file to Data
+        let dataPdf = try? Data(contentsOf: pathPdf)
+        // show data into web view
+        vistaWeb.load(dataPdf!, mimeType: "application/pdf", characterEncodingName: "utf-8", baseURL: pathPdf)
     }
     
 }
